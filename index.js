ae5e88cffa3df13e377bc45da7ad323d5983296b
omkarkakad <139750149+omkarkakad@users.noreply.github.com>
2024-07-18 09:47:21 +0530

    Add files via upload

---
+++ index.js
@@ -0,0 +1,21 @@
+const express=require("express");
+const cors=require("cors");
+const { MongoClient }=require("mongodb");
+
+
+const app=express();
+app.use(cors());
+app.use(express.json());
+
+app.post("/save",(req,res)=>{
+	const url="mongodb+srv://kakadomkar03:ax1RT1toPyIsOeV1@cluster0.u1mpze8.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0";
+	const client=new MongoClient(url);
+	const db=client.db("wn18july24");
+	const coll=db.collection("student");
+	const document={"name":req.body.name, "choice":req.body.choice};
+	coll.insertOne(document)
+	.then(result=>res.send(result))
+	.catch(error=>res.send(error));
+
+});
+app.listen(9000, () =>{ console.log("ready @ 9000");});
